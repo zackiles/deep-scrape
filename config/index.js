05a439f7a2623a8e527c798a95b0544bc52d026d
Zachary Iles <zackiles@users.noreply.github.com>
2015-08-25 11:38:55 -0400

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ if (typeof process.env.NODE_ENV === 'undefined') process.env.NODE_ENV = 'develop
 
 module.exports = {
   env: process.env.NODE_ENV,
-  root: path.normalize(__dirname + '/../../..'),
+  root: path.normalize(__dirname + '/..'),
   browserOptions: {
     blockedResources: require('./blocked-resources.json')
   }
